c832a28121d2f872e5edd5244618621c5210b60e
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-04-28 11:52:17 -0500

    Requerimento 4

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -11,6 +11,14 @@ describe ("Unit Test for User class", () => {
     expect(user.bio).toBe("Bio")
     expect(user.dateCreated).not.toBeUndefined()
     expect(user.lastUpdated).not.toBeUndefined()
-});
+    });
+
+    test ('Add gettert', () => {
+        const user = new User(1,"carlogilmar", "Carlo", "Bio")
+        expect(user.getUsername).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    });
 
 })
