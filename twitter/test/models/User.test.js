8369d8528f29628bb7e61d769f22338b1dc23e9f
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-04-28 11:46:37 -0500

    Requerimento 2

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -3,14 +3,14 @@ const User = require('./../../app/models/User')
 describe ("Unit Test for User class", () => {
     test ('Create an User Object', () => {
     //Código a utilizar
-    const user = new User (1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+    const user = new User (1, "carlogilmar", "Carlo", "Bio")
     //Validación
     expect(user.id).toBe(1)
     expect(user.username).toBe("carlogilmar")
     expect(user.name).toBe("Carlo")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
 });
 
 })
